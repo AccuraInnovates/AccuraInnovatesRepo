7108461a09623dc552b4b3e8e2492d1c8b34233f
AccuraInnovates <accurainnovates@gmail.com>
2024-03-28 17:06:52 +0530

    check

--- obj/Debug/net8.0/Accura_Innovatives.AssemblyInfo.cs
+++ obj/Debug/net8.0/Accura_Innovatives.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Accura_Innovatives")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3726604c12c159c81490a21f41b14a3665341631")]
 [assembly: System.Reflection.AssemblyProductAttribute("Accura_Innovatives")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Accura_Innovatives")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
